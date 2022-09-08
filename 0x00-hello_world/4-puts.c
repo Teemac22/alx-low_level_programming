ec1adbe1b92a5724f9a5eb2d3fa3fbe1208303ee
Teemac22 <tosinmacaulay47@gmail.com>
2022-09-08 10:47:34 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,26 @@
-#!/bin/bash
-<<<<<<< HEAD
-#include <stdio.h>/***main - entry point** Return: 0 after printing the function*/ int main(void){puts("\""Programming is like building a multilingual puzzle"); return(0);}
-=======
-#include <stdio.h>
-/**
- * *main - entry point
- *
- * Return: 0 after printing the function 
- */
-int main(void)
-{puts("\""Programming is like building a multilingual puzzle"); return(0);}
->>>>>>> Task 4
+#include <stdio.h>                                                                                                          
+
+                                                                                                                            
+
+/**                                                                                                                         
+
+* main -print the string in the put function                                                                                
+
+*                                                                                                                           
+
+* Description using the main function                                                                                       
+
+* this program prints "Programming is like building a multilingual puzzle                                                   
+
+* Return: 0                                                                                                                 
+
+*/                                                                                                                          
+
+int main(void)                                                                                                              
+
+{                                                                                                                           
+
+        puts("\"Programming is like building a multilingual puzzle");                                                       
+
+        return (0);
+}
