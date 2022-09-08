630486a2068563286e0371725bc29feed5b4609b
Teemac22 <tosinmacaulay47@gmail.com>
2022-09-08 06:22:32 -0700

    Task 4 corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
 #!/bin/bash
+<<<<<<< HEAD
 #include <stdio.h>/***main - entry point** Return: 0 after printing the function*/ int main(void){puts("\""Programming is like building a multilingual puzzle"); return(0);}
+=======
+#include <stdio.h>
+/**
+ * *main - entry point
+ *
+ * Return: 0 after printing the function 
+ */
+int main(void)
+{puts("\""Programming is like building a multilingual puzzle"); return(0);}
+>>>>>>> Task 4
