c311f6ef641c37a762a8709a095868d5cfddddbf
Teemac22 <111114829+Teemac22@users.noreply.github.com>
2022-09-21 20:48:56 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,17 +5,18 @@
  * @str: The string to be changed.                                                                                                   
  *                                                                                                                                   
  * Return: A pointer to the changed string.                                                                                          
- */                                                                                                                                  
+ */ 
+
 char *string_toupper(char *str)                                                                                                      
 {                                                                                                                                    
-        int index = 0;                                                                                                               
+        int i = 0;                                                                                                               
                                                                                                                                      
-        while (str[index])                                                                                                           
+        while (str[i] != '\0')                                                                                                           
         {                                                                                                                            
-                if (str[index] >= 'a' && str[index] <= 'z')                                                                          
-                        str[index] -= 32;                                                                                            
+                if (str[i] >= 'a' && str[i] <= 'z')                                                                          
+                        str[i] -= 32;                                                                                            
                                                                                                                                      
-                index++;                                                                                                             
+                i++;                                                                                                             
         }                                                                                                                            
                                                                                                                                      
         return (str);                                                                                                                
