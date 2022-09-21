e3d6928287280d49bdaa9776ef612e3ee6c42ed5
Teemac22 <111114829+Teemac22@users.noreply.github.com>
2022-09-21 19:42:32 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 /**
  * _strncat - concatenate two strings but add inputted number of bytes
  * @dest: string to be appended upon
- * @src: string to be completed tat the end of dest
+ * @src: string to be completed at the end of dest
  * @n:integer parameter to compare index to
  * Return: returns new concatenated string
  */
