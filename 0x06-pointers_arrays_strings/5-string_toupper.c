a62c67a5eefa897da467f79de707dfbebbaeee41
Teemac22 <111114829+Teemac22@users.noreply.github.com>
2022-09-22 10:20:30 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,21 @@
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * _strcmp - Compares pointers to two strings.                                                                                       
- * @s1: A pointer to the first string to be compared.                                                                                
- * @s2: A pointer to the second string to be compared.                                                                               
- *                                                                                                                                   
- * Return: If str1 < str2, the negative difference of the first unmatched char                                                       
- *         If str1 == str2, 0.                                                                                                       
- *         If str1 > str2, the positive difference of the first unmatched char                                                       
- */                                                                                                                                  
-int _strcmp(char *s1, char *s2)                                                                                                      
-{                                                                                                                                    
-        while (*s1 && *s2 && *s1 == *s2)                                                                                             
-        {                                                                                                                            
-                s1++;                                                                                                                
-                s2++;                                                                                                                
-        }                                                                                                                            
-                                                                                                                                     
-        return (*s1 - *s2);                                                                                                          
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @str: string to be changed
+ *
+ * Return: address tp the string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
 }
