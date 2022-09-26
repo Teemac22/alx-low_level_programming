1119e9089493c848650eebc40f77e1fb0f18c85c
Teemac22 <111114829+Teemac22@users.noreply.github.com>
2022-09-26 12:07:48 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H                                                                                                                     
-#define MAIN_H                                                                                                                     
-                                                                                                                                   
-/**                                                                                                                                
- * File: main.h                                                                                                                    
- * Auth: sam tech (samuel birhanu)                                                                                                 
- * description: Header file containing prototypes for all functions                                                                
- *       used in the 0x06-pointers_arrays_strings directory.                                                                       
- */                                                                                                                                
-                                                                                                                                   
+#define MAIN_H                                                                                                                  
+
 #include <stddef.h>                                                                                                                
                                                                                                                                    
 int _putchar(char c);                                                                                                              
