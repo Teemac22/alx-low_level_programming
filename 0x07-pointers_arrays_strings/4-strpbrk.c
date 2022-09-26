90422eda26a6d73d76b4ac6004e8b976150eff75
Teemac22 <111114829+Teemac22@users.noreply.github.com>
2022-09-26 13:40:36 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,27 @@
-#include "main.h"
-
-/**
- * _strpbrk - searches a string for any set of bytes
- *@s: string to check
- *@accept: string to check against
- *
- *Return: pointer to byte in s that matches or NULL if no match
- */
-char *_strpbrk(char *s, char *accept)
-{
-	unsigned int i, j;
-
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; accept[j]; j++)
-		{
-			 if (s[i] == accept[j])	
-				break;
-		}
-		if (accept[j])
-			return (s + i);
-	}
-	return (0);
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * _strpbrk - Searches a string for any of a set of bytes.                                                                           
+ * @s: The string to be searched.                                                                                                    
+ * @accept: The set of bytes to be searched for.                                                                                     
+ *                                                                                                                                   
+ * Return: If a set is matched - a pointer to the matched byte.                                                                      
+ *         If no set is matched - NULL.                                                                                              
+ */                                                                                                                                  
+char *_strpbrk(char *s, char *accept)                                                                                                
+{                                                                                                                                    
+        int index;                                                                                                                   
+                                                                                                                                     
+        while (*s)                                                                                                                   
+        {                                                                                                                            
+                for (index = 0; accept[index]; index++)                                                                              
+                {                                                                                                                    
+                        if (*s == accept[index])                                                                                     
+                                return (s);                                                                                          
+                }                                                                                                                    
+                                                                                                                                     
+                s++;                                                                                                                 
+        }                                                                                                                            
+                                                                                                                                     
+        return ('\0');                                                                                                               
 }
