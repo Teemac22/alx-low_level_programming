11d04a0c386ea4b900cdcdf4fa0d890a3dd7a8ed
Teemac22 <111114829+Teemac22@users.noreply.github.com>
2022-09-27 09:03:07 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,13 +2,16 @@
 #define MAIN_H
 
 /**
+ * File: main.h
+ * Auth: sam tech (samuel birhanu)
+ * description: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
 
-* void prototypes()
-* int prototypes()
-*/
+#include <stddef.h>
 
-int _putchar(char);
-char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -18,4 +21,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /*MAIN_H*/
+#endif
